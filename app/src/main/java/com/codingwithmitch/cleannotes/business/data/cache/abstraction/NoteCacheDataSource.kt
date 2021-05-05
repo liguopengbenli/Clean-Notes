fa2a694ad3ea8ac86cc140471489f136b920ab82
guopeng li <guopeng.li@technicolor.com>
2021-05-05 13:40:18 +0200

    Data Layer (Network)

---
+++ app/src/main/java/com/codingwithmitch/cleannotes/business/data/cache/abstraction/NoteCacheDataSource.kt
@@ -0,0 +1,20 @@
+package com.codingwithmitch.cleannotes.business.data.cache.abstraction
+
+import com.codingwithmitch.cleannotes.business.domain.model.Note
+
+interface NoteCacheDataSource {
+    suspend fun insertNote(note: Note): Long
+    suspend fun deleteNote(primary: String): Int
+    suspend fun deleteNotes(note: List<Note>): Int
+    suspend fun updateNote(primary: String, newTitle: String, newBody: String): Int
+    suspend fun searchNotes(
+        query: String,
+        filterAndOrder: String,
+        page: Int
+    ): List<Note>
+
+    suspend fun searchNoteById(primaryKey: String): Note?
+    suspend fun getNumNotes(): Int
+
+    suspend fun insertNotes(notes: List<Note>): LongArray
+}
