c6bfe66fd228a59799cb43ea708bef88fda735eb
guopeng li <guopeng.li@technicolor.com>
2021-05-08 16:36:17 +0200

    InsertNewNote Unit Tests part2

--- app/src/test/java/com/codingwithmitch/cleannotes/business/interactors/notelist/insertNewNoteTest.kt
+++ app/src/test/java/com/codingwithmitch/cleannotes/business/interactors/notelist/insertNewNoteTest.kt
@@ -1,10 +1,14 @@
 package com.codingwithmitch.cleannotes.business.interactors.notelist
 
 
+import com.codingwithmitch.cleannotes.business.data.cache.CacheErrors
+import com.codingwithmitch.cleannotes.business.data.cache.FORCE_GENERAL_FAILURE
+import com.codingwithmitch.cleannotes.business.data.cache.FORCE_NEW_NOTE_EXCEPTION
 import com.codingwithmitch.cleannotes.business.data.cache.abstraction.NoteCacheDataSource
 import com.codingwithmitch.cleannotes.business.data.network.abstraction.NoteNetworkDataSource
 import com.codingwithmitch.cleannotes.business.domain.model.NoteFactory
 import com.codingwithmitch.cleannotes.business.domain.state.DataState
+import com.codingwithmitch.cleannotes.business.interactors.notelist.InsertNewNote.Companion.INSERT_NOTE_FAILED
 import com.codingwithmitch.cleannotes.business.interactors.notelist.InsertNewNote.Companion.INSERT_NOTE_SUCCESS
 import com.codingwithmitch.cleannotes.di.DependencyContainer
 import com.codingwithmitch.cleannotes.framework.presentation.notelist.state.NoteListStateEvent
@@ -90,5 +94,61 @@ class InsertNewNoteTest {
         val networkNoteThatWasInserted = noteNetworkDataSource.searchNote(newNote)
         assertTrue(networkNoteThatWasInserted == newNote)
     }
+
+    @Test
+    fun insertNote_fail_confirmNetworkAndCacheUnchanged() = runBlocking {
+        val newNote = noteFactory.createSingleNote(
+            id = FORCE_GENERAL_FAILURE,
+            title = UUID.randomUUID().toString()
+        )
+
+        insertNewNote.insertNewNote(
+            id = newNote.id,
+            title = newNote.title,
+            stateEvent = NoteListStateEvent.InsertNewNoteEvent(title = newNote.title)
+        ).collect(object : FlowCollector<DataState<NoteListViewState>?>{
+            override suspend fun emit(value: DataState<NoteListViewState>?) {
+                assertEquals(
+                    value?.stateMessage?.response?.message,
+                    INSERT_NOTE_FAILED
+                )
+            }
+        })
+
+        val cacheNoteThatWasInserted = noteCacheDataSource.searchNoteById(newNote.id)
+        assertTrue{cacheNoteThatWasInserted == null}
+
+        val networkNoteThatWasInserted = noteNetworkDataSource.searchNote(newNote)
+        assertTrue(networkNoteThatWasInserted == null)
+    }
+
+    @Test
+    fun throwException_checkGenericError_confirmNetworkAndCacheUnchanged() = runBlocking {
+        val newNote = noteFactory.createSingleNote(
+            id = FORCE_NEW_NOTE_EXCEPTION,
+            title = UUID.randomUUID().toString()
+        )
+
+        insertNewNote.insertNewNote(
+            id = newNote.id,
+            title = newNote.title,
+            stateEvent = NoteListStateEvent.InsertNewNoteEvent(title = newNote.title)
+        ).collect(object : FlowCollector<DataState<NoteListViewState>?>{
+            override suspend fun emit(value: DataState<NoteListViewState>?) {
+                assert(
+                    value?.stateMessage?.response?.message?.contains(CacheErrors.CACHE_ERROR_UNKNOWN)?:false
+                )
+            }
+        })
+
+        val cacheNoteThatWasInserted = noteCacheDataSource.searchNoteById(newNote.id)
+        assertTrue{cacheNoteThatWasInserted == null}
+
+        val networkNoteThatWasInserted = noteNetworkDataSource.searchNote(newNote)
+        assertTrue(networkNoteThatWasInserted == null)
+
+    }
+
+
 
     }
