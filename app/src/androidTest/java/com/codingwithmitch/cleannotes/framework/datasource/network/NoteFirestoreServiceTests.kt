e6604c76d9b46e2de6f59a9bb92b747a6462fccb
guopeng li <guopeng.li@technicolor.com>
2021-05-14 17:45:49 +0200

    firestore test

--- app/src/androidTest/java/com/codingwithmitch/cleannotes/framework/datasource/network/NoteFirestoreServiceTests.kt
+++ app/src/androidTest/java/com/codingwithmitch/cleannotes/framework/datasource/network/NoteFirestoreServiceTests.kt
@@ -1,7 +1,9 @@
 package com.codingwithmitch.cleannotes.framework.datasource.network
 import android.content.Context
+import android.util.Log
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.codingwithmitch.cleannotes.business.domain.model.Note
 import com.codingwithmitch.cleannotes.business.domain.model.NoteFactory
 import com.codingwithmitch.cleannotes.di.TestAppComponent
 import com.codingwithmitch.cleannotes.framework.BaseTest
@@ -22,8 +24,25 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.*
 import javax.inject.Inject
+import kotlin.random.Random
+import kotlin.random.Random.Default.nextInt
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
+
+
+/*
+LEGEND:
+1. CBS = "Confirm by searching"
+Test cases:
+1. insert a single note, CBS
+2. update a random note, CBS
+3. insert a list of notes, CBS
+4. delete a single note, CBS
+5. insert a deleted note into "deletes" node, CBS
+6. insert a list of deleted notes into "deletes" node, CBS
+7. delete a 'deleted note' (note from "deletes" node). CBS
+ */
 
 
 @RunWith(AndroidJUnit4ClassRunner::class)
@@ -101,11 +120,134 @@ class NoteFirestoreServiceTests: BaseTest(){
     }
 
     @Test
-    fun queryAllNotes() = runBlocking {
-        val notes = noteFirestoreService.getAllNotes()
-        printLogD("FirestoreServiceTests", "notes: ${notes.size}")
-        assertTrue { notes.size > 5 }
+    fun updateSingleNote_CBS() = runBlocking{
+
+        val searchResults = noteFirestoreService.getAllNotes()
+
+        // choose a random note from list to update
+        val randomNote = searchResults.get(Random.nextInt(0,searchResults.size-1) + 1)
+        val UPDATED_TITLE = UUID.randomUUID().toString()
+        val UPDATED_BODY = UUID.randomUUID().toString()
+        var updatedNote = noteDataFactory.createSingleNote(
+            id = randomNote.id,
+            title = UPDATED_TITLE,
+            body = UPDATED_BODY
+        )
+
+        noteFirestoreService.insertOrUpdateNote(updatedNote)
+
+        updatedNote = noteFirestoreService.searchNote(updatedNote)!!
+
+        assertEquals(UPDATED_TITLE, updatedNote.title)
+        assertEquals(UPDATED_BODY, updatedNote.body)
     }
+
+    @Test
+    fun insertNoteList_CBS() = runBlocking {
+        val list = noteDataFactory.produceListOfNotes()
+
+        noteFirestoreService.insertOrUpdateNotes(list)
+
+        val searchResults = noteFirestoreService.getAllNotes()
+
+        //Log.d("insertNoteList_CBS", "list = ${list}")
+        Log.d("insertNoteList_CBS", "searchResults = ${searchResults}")
+        // we have different time creation problem syncronisation
+        assertTrue { searchResults.containsAll(list) }
+    }
+
+    @Test
+    fun deleteSingleNote_CBS() = runBlocking {
+        val noteList = noteFirestoreService.getAllNotes()
+
+        // choose one at random to delete
+        val noteToDelete = noteList.get(Random.nextInt(0, noteList.size - 1) + 1)
+
+        noteFirestoreService.deleteNote(noteToDelete.id)
+
+        // confirm it no longer exists in firestore
+        val searchResults = noteFirestoreService.getAllNotes()
+
+        assertFalse { searchResults.contains(noteToDelete) }
+    }
+
+    @Test
+    fun insertIntoDeletesNode_CBS() = runBlocking {
+        val noteList = noteFirestoreService.getAllNotes()
+
+        // choose one at random to insert into "deletes" node
+        val noteToDelete = noteList.get(Random.nextInt(0, noteList.size - 1) + 1)
+
+        noteFirestoreService.insertDeletedNote(noteToDelete)
+
+        // confirm it is now in the "deletes" node
+        val searchResults = noteFirestoreService.getDeletedNote()
+
+        assertTrue { searchResults.contains(noteToDelete) }
+    }
+
+    @Test
+    fun insertListIntoDeletesNode_CBS() = runBlocking {
+        val noteList = ArrayList(noteFirestoreService.getAllNotes())
+
+        // choose some random notes to add to "deletes" node
+        val notesToDelete: ArrayList<Note> = ArrayList()
+
+        // 1st
+        var noteToDelete = noteList.get(Random.nextInt(0, noteList.size - 1) + 1)
+        noteList.remove(noteToDelete)
+        notesToDelete.add(noteToDelete)
+
+        // 2nd
+        noteToDelete = noteList.get(Random.nextInt(0, noteList.size - 1) + 1)
+        noteList.remove(noteToDelete)
+        notesToDelete.add(noteToDelete)
+
+        // 3rd
+        noteToDelete = noteList.get(Random.nextInt(0, noteList.size - 1) + 1)
+        noteList.remove(noteToDelete)
+        notesToDelete.add(noteToDelete)
+
+        // 4th
+        noteToDelete = noteList.get(Random.nextInt(0, noteList.size - 1) + 1)
+        noteList.remove(noteToDelete)
+        notesToDelete.add(noteToDelete)
+
+        // insert into "deletes" node
+        noteFirestoreService
+            .insertDeletedNotes(notesToDelete)
+
+        // confirm the notes are in "deletes" node
+        val searchResults = noteFirestoreService.getDeletedNote()
+
+        assertTrue { searchResults.containsAll(notesToDelete) }
+    }
+
+    @Test
+    fun deleteDeletedNote_CBS() = runBlocking {
+        val note = noteDataFactory.createSingleNote(
+            UUID.randomUUID().toString(),
+            UUID.randomUUID().toString(),
+            UUID.randomUUID().toString()
+        )
+
+        // insert into "deletes" node
+        noteFirestoreService.insertDeletedNote(note)
+
+        // confirm note is in "deletes" node
+        var searchResults = noteFirestoreService.getDeletedNote()
+
+        assertTrue { searchResults.contains(note) }
+
+        // delete from "deletes" node
+        noteFirestoreService.deleteDeletedNote(note)
+
+        // confirm note is deleted from "deletes" node
+        searchResults = noteFirestoreService.getDeletedNote()
+
+        assertFalse { searchResults.contains(note) }
+    }
+
 
     companion object{
         const val EMAIL = "1042332611@qq.com"
