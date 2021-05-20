8d0cf856d03672b8ccb79abc0c036610e689718e
guopeng li <guopeng.li@technicolor.com>
2021-05-20 10:13:41 +0200

    NoteList Toolbar States and RecyclerView Item Selection

--- app/src/main/java/com/codingwithmitch/cleannotes/framework/presentation/notelist/NoteListViewModel.kt
+++ app/src/main/java/com/codingwithmitch/cleannotes/framework/presentation/notelist/NoteListViewModel.kt
@@ -2,17 +2,21 @@ package com.codingwithmitch.cleannotes.framework.presentation.notelist
 
 import android.content.SharedPreferences
 import android.os.Parcelable
+import androidx.lifecycle.LiveData
 import com.codingwithmitch.cleannotes.business.domain.model.Note
 import com.codingwithmitch.cleannotes.business.domain.model.NoteFactory
 import com.codingwithmitch.cleannotes.business.domain.state.*
+import com.codingwithmitch.cleannotes.business.interactors.notelist.DeleteMultipleNotes.Companion.DELETE_NOTES_YOU_MUST_SELECT
 import com.codingwithmitch.cleannotes.business.interactors.notelist.NoteListInteractors
 import com.codingwithmitch.cleannotes.framework.datasource.cache.database.NOTE_FILTER_DATE_CREATED
 import com.codingwithmitch.cleannotes.framework.datasource.cache.database.NOTE_ORDER_DESC
 import com.codingwithmitch.cleannotes.framework.datasource.preferences.PreferenceKeys.Companion.NOTE_FILTER
 import com.codingwithmitch.cleannotes.framework.datasource.preferences.PreferenceKeys.Companion.NOTE_ORDER
 import com.codingwithmitch.cleannotes.framework.presentation.common.BaseViewModel
+import com.codingwithmitch.cleannotes.framework.presentation.notelist.state.NoteListInteractionManager
 import com.codingwithmitch.cleannotes.framework.presentation.notelist.state.NoteListStateEvent
 import com.codingwithmitch.cleannotes.framework.presentation.notelist.state.NoteListStateEvent.*
+import com.codingwithmitch.cleannotes.framework.presentation.notelist.state.NoteListToolbarState
 import com.codingwithmitch.cleannotes.framework.presentation.notelist.state.NoteListViewState
 import com.codingwithmitch.cleannotes.util.printLogD
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -31,6 +35,32 @@ constructor(
     private val editor: SharedPreferences.Editor,
     private val sharedPreferences: SharedPreferences
 ): BaseViewModel<NoteListViewState>(){
+
+    val noteListInteractionManager =
+        NoteListInteractionManager()
+
+    val toolbarState: LiveData<NoteListToolbarState>
+        get() = noteListInteractionManager.toolbarState
+
+    /*
+        State
+    */
+    fun getSelectedNotes() = noteListInteractionManager.getSelectedNotes()
+
+    fun setToolbarState(state: NoteListToolbarState)
+            = noteListInteractionManager.setToolbarState(state)
+
+    fun isMultiSelectionStateActive()
+            = noteListInteractionManager.isMultiSelectionStateActive()
+
+    fun addOrRemoveNoteFromSelectedList(note: Note)
+            = noteListInteractionManager.addOrRemoveNoteFromSelectedList(note)
+
+    fun isNoteSelected(note: Note): Boolean
+            = noteListInteractionManager.isNoteSelected(note)
+
+    fun clearSelectedNotes() = noteListInteractionManager.clearSelectedNotes()
+
 
     init {
         setNoteFilter(
@@ -405,6 +435,33 @@ constructor(
     fun refreshSearchQuery(){
         setQueryExhausted(false)
         setStateEvent(SearchNotesEvent(false))
+    }
+
+    private fun removeSelectedNotesFromList(){
+        val update = getCurrentViewStateOrNew()
+        update.noteList?.removeAll(getSelectedNotes())
+        setViewState(update)
+        clearSelectedNotes()
+    }
+
+    fun deleteNotes(){
+        if(getSelectedNotes().size > 0){
+            setStateEvent(DeleteMultipleNotesEvent(getSelectedNotes()))
+            removeSelectedNotesFromList()
+        }
+        else{
+            setStateEvent(
+                CreateStateMessageEvent(
+                    stateMessage = StateMessage(
+                        response = Response(
+                            message = DELETE_NOTES_YOU_MUST_SELECT,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Info()
+                        )
+                    )
+                )
+            )
+        }
     }
 
 }
